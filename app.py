0b6e3527328621fede4b4dfbe712797ecc6663af
mttble <matthew.beale1@hotmail.com>
2023-06-06 11:53:13 +0930

    Register User complete

--- app.py
+++ app.py
@@ -1,19 +1,36 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 from datetime import date
 from flask_marshmallow import Marshmallow
+from flask_bcrypt import Bcrypt
 
 app = Flask(__name__)
 
-# app.config['JSON_SORT_KEYS'] = False
+app.config['JSON_SORT_KEYS'] = False
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql+psycopg2://trello_dev:matt@localhost:5432/trello'
 
 db = SQLAlchemy(app)
-ma = Marshmallow(app) # allows for jsonifying
+ma = Marshmallow(app)
+bcrypt = Bcrypt(app)
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    email = db.Column(db.String, nullable=False, unique=True)
+    password = db.Column(db.String, nullable=False)
+    is_admin = db.Column(db.Boolean, default=False)
+
+class UserSchema(ma.Schema):
+    class Meta:
+        fields = ('name', 'email', 'is_admin')
+
 
 class Card(db.Model):
-    __tablename__ = 'cards'
+    __tablename__ = "cards"
+
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
     description = db.Column(db.Text())
@@ -21,62 +38,91 @@ class Card(db.Model):
     date_created = db.Column(db.Date())
 
 class CardSchema(ma.Schema):
-    class Meta:
-        fields = ('id', 'title', 'description', 'status')
-        
+  class Meta:
+    fields = ('id', 'title', 'description', 'status')
+    ordered = True
 
-@app.cli.command('create')
+@app.cli.command("create")
 def create_db():
     db.drop_all()
     db.create_all()
-    print('Tables Created Successfully')
+    print("Tables created successfully")
 
-@app.cli.command('seed')
+
+@app.cli.command("seed")
 def seed_db():
+    users = [
+        User(
+            email='admin@spam.com',
+            password=bcrypt.generate_password_hash('spinynorman').decode('utf-8'),
+            is_admin=True
+        ),
+        User(
+            name='John Cleese',
+            email='cleese@spam.com',
+            password=bcrypt.generate_password_hash('tisbutascratch').decode('utf-8')
+        )
+    ]
+
     # Create an instance of the Card model in memory
     cards = [
         Card(
-            title = 'Start the project',
-            description = 'Stage 1 - Create ERD',
-            status = 'Done',
-            date_created = date.today()
-    ),
+            title="Start the project",
+            description="Stage 1 - Create an ERD",
+            status="Done",
+            date_created=date.today(),
+        ),
         Card(
-            title = 'ORM Queries',
-            description = 'Stage 2 - Implement several queries',
-            status = 'In Progress',
-            date_created = date.today()
-    ),
+            title="ORM Queries",
+            description="Stage 2 - Implement several queries",
+            status="In Progress",
+            date_created=date.today(),
+        ),
         Card(
-            title = 'Marshmallow',
-            description = 'Stage 3 - Implement jsonify of models',
-            status = 'In Progress',
-            date_created = date.today()
-    )
+            title="Marshmallow",
+            description="Stage 3 - Implement jsonify of models",
+            status="In Progress",
+            date_created=date.today(),
+        ),
     ]
-    # Truncate the Card table (clear out table)
-    db.session.query(Card).delete()
 
-    #  Add the card to the sessions (transaction)
+    # Truncate the Card table
+    db.session.query(Card).delete()
+    db.session.query(User).delete()
+
+    # Add the card to the session (transaction)
     db.session.add_all(cards)
+    db.session.add_all(users)
 
     # Commit the transaction to the database
     db.session.commit()
-    print('Models seeded')
+    print("Models seeded")
+
+
+@app.route('/register', methods=['POST'])
+def register():
+    user_info = UserSchema().load(request.json)
+    user = User(
+        email=user_info.email,
+        password=bcrypt.generate_password_hash(user_info.password).decode('utf8'),
+        name=user_info.name
+    )
+    print(user)
+    return {}
+
 
 @app.route('/cards')
 def all_cards():
-    # select * from cards; (stmt is statement)
-    #(db.or_(Card.status != 'Done', Card.id > 2)) - or function card status is not Done or card ID greater than 2
-    stmt = db.select(Card).order_by(Card.status.desc())
-    cards = db.session.scalars(stmt).all()
-    return CardSchema(many=True).dumps(cards)
-    # for card in cards:
-    #     print(card.__dict__)
+  # select * from cards;
+  stmt = db.select(Card).order_by(Card.status.desc())
+  cards = db.session.scalars(stmt).all()
+  return CardSchema(many=True).dump(cards)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return "Hello, world!"
+    return "Hello World!"
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     app.run(debug=True)
