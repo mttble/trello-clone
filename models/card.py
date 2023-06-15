4e43bb0ee635b5ed0e73385be28b8e9ba469d30e
mttble <matthew.beale1@hotmail.com>
2023-06-15 12:47:06 +0930

    Validation complete

--- models/card.py
+++ models/card.py
@@ -1,5 +1,10 @@
 from init import db, ma
-from marshmallow import fields
+from marshmallow import fields, validates_schema
+from marshmallow.validate import Length, OneOf, And, Regexp
+from marshmallow.exceptions import ValidationError
+
+# Validation for status field doing OneOf
+VALID_STATUSES = ['To Do', 'Done', 'In Progress', 'Testing', 'Deployed']
 
 class Card(db.Model):
     __tablename__ = "cards"
@@ -22,7 +27,20 @@ class CardSchema(ma.Schema):
   # wrapped in a list because many comments
   comments = fields.List(fields.Nested('CommentSchema', exclude=['card', 'id']))
   # validation
-  title = fields.String(required=True)
+  title = fields.String(required=True, validate=And(
+     Length(min=3, error='Title must be at least 3 characters long'),
+     Regexp('^[a-zA-Z0-9 ]+$', error='Only letters, numbers and spaces are allowed')
+     ))
+  description = fields.String(load_default='')
+  status = fields.String(load_default=VALID_STATUSES[0])
+  # status = fields.String(load_default=VALID_STATUSES[0], validate=OneOf(VALID_STATUSES))
+
+  @validates_schema()
+  def validate_status(self, data, **kwargs):
+     status = [x for x in VALID_STATUSES if x.upper() == data['status'].upper()]
+     if len(status) == 0:
+        raise ValidationError(f'Status must be one of: {VALID_STATUSES}')
+     data['status'] = status[0]
 
   class Meta:
     # add user and comments for jsonify output in marshmallow
